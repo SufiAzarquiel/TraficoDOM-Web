acbc9ca034a9cc2c4005669be214ac738ed9712e
Sufi <116170704+SufiAzarquiel@users.noreply.github.com>
2023-10-03 14:08:12 +0200

    Update WeatherPerHour.java

--- src/main/java/parser/WeatherPerHour.java
+++ src/main/java/parser/WeatherPerHour.java
@@ -4,58 +4,57 @@ import java.util.ArrayList;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import model.Intervalo;
+import model.Incidencia;
 
 public class WeatherPerHour {
-	ArrayList<Intervalo> intervalos;
-	
+	ArrayList<Incidencia> incidencias;
+
 	public WeatherPerHour() {
-		DOMParserWeb parser = new DOMParserWeb("https://api.openweathermap.org/data/2.5/forecast?q=Toledo,es&mode=xml&APPID=601c9db344b44f9774ef76a4c07979b1&lang=sp");
+		DOMParserWeb parser = new DOMParserWeb(
+				"https://api.openweathermap.org/data/2.5/forecast?q=Toledo,es&mode=xml&APPID=601c9db344b44f9774ef76a4c07979b1&lang=sp");
 		Document doc = parser.getDocumento();
-		
-		intervalos = new ArrayList<Intervalo>();
-		
+
+		incidencias = new ArrayList<Incidencia>();
+
 		// root
 		Element weatherData = (Element) doc.getElementsByTagName("forecast").item(0);
-		
+
 		// root/time
-		NodeList intervalosRaw = weatherData.getElementsByTagName("time");
-		for (int i = 0; i < intervalosRaw.getLength(); i++) {
-			Element nodoIntervalo = (Element) intervalosRaw.item(i);
-		    Intervalo intervalo = new Intervalo();
-			
-			// time/symbol	
-		    Element simboloData = (Element) nodoIntervalo.getElementsByTagName("symbol").item(0);
-		    // time/symbol@var
-			intervalo.setSimbolo(simboloData.getAttribute("var"));
+		NodeList incidenciasRaw = weatherData.getElementsByTagName("time");
+		for (int i = 0; i < incidenciasRaw.getLength(); i++) {
+			Element nodoIncidencia = (Element) incidenciasRaw.item(i);
+			Incidencia incidencia = new Incidencia();
+
+			// time/symbol
+			Element simboloData = (Element) nodoIncidencia.getElementsByTagName("symbol").item(0);
+			// time/symbol@var
+			incidencia.setSimbolo(simboloData.getAttribute("var"));
 
 			// time/precipitation
-			Element precData = (Element) nodoIntervalo.getElementsByTagName("precipitation").item(0);
+			Element precData = (Element) nodoIncidencia.getElementsByTagName("precipitation").item(0);
 			// time/precipitation@probability
 			Float probF = Float.parseFloat(simboloData.getAttribute("probability")) * 10;
 			int probInt = Math.round(probF);
-			intervalo.setPrecipitacion(probInt);
-			
+			incidencia.setPrecipitacion(probInt);
+
 			// time/temperature
-			Element temperData = (Element) nodoIntervalo.getElementsByTagName("temperature").item(0);
+			Element temperData = (Element) nodoIncidencia.getElementsByTagName("temperature").item(0);
 			// time/temperature@min
-			intervalo.setTemperaturaMinima(Float.parseFloat(temperData.getAttribute("min")));
+			incidencia.setTemperaturaMinima(Float.parseFloat(temperData.getAttribute("min")));
 			// time/temperature@max
-			intervalo.setTemperaturaMinima(Float.parseFloat(temperData.getAttribute("max")));
-			
+			incidencia.setTemperaturaMinima(Float.parseFloat(temperData.getAttribute("max")));
+
 			// time/clouds
-			Element cloudData = (Element) nodoIntervalo.getElementsByTagName("clouds").item(0);
+			Element cloudData = (Element) nodoIncidencia.getElementsByTagName("clouds").item(0);
 			// time/clouds/@value
-			
-			
-			intervalos.add(intervalo);
+
+			incidencias.add(incidencia);
 		}
 	}
 
-	public ArrayList<Intervalo> getIntervalos() {
-		return intervalos;
+	public ArrayList<Incidencia> getIncidencias() {
+		return incidencias;
 	}
 }
